fa1c6880c041b4ad6147f51c710c8a11ca46a19d
carmona2712 <carmonaricardo9511@gmail.com>
2021-11-22 00:33:55 -0500

    Terminado Session de Zuul server con Eureka

--- src/main/java/com/raca/ms_item/controllers/ItemController.java
+++ src/main/java/com/raca/ms_item/controllers/ItemController.java
@@ -1,8 +1,10 @@
 package com.raca.ms_item.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.raca.ms_item.models.entity.Item;
+import com.raca.ms_item.models.entity.Product;
 import com.raca.ms_item.models.service.Impl.ItemFeignService;
-import com.raca.ms_item.models.service.Impl.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import java.util.List;
 
@@ -23,14 +24,31 @@ public class ItemController {
 
     @GetMapping("/getAll")
     public ResponseEntity<List<Item>> getAll() {
-        return new ResponseEntity(itemService.findAll(),HttpStatus.OK);
+        return new ResponseEntity(itemService.findAll().getBody(),HttpStatus.OK);
     }
 
+
+    @HystrixCommand(fallbackMethod = "findById")
     @GetMapping("/{id}")
     public ResponseEntity<Item> getById(@PathVariable("id") Long id) {
         System.out.println(itemService.findById(id, 2).toString());
         try{
             return new ResponseEntity(itemService.findById(id, 2),HttpStatus.OK);
+        }catch(Exception ex){
+            return new ResponseEntity(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Item> findById(Long id) {
+       Item item = new Item();
+       Product product = new Product();
+       product.setId(id);
+       product.setName("TV SAMSUMG 65");
+       product.setCost(Double.parseDouble("2368000"));
+       item.setProduct(product);
+       item.setQuantity(4);
+        try{
+            return new ResponseEntity(item,HttpStatus.OK);
         }catch(Exception ex){
             return new ResponseEntity(null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
